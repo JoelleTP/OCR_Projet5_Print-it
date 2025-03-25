6a91d72f731b1b5e76ae8aeb98d975c9f3da300e
JoelleTP <joe.pflieg@hotmail.fr>
2025-03-25 15:42:23 +0100

    Etape 3: modification du script.js pour ajout des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -29,3 +29,26 @@ leftarrow.addEventListener("click", () => {
 	console.log("J'ai cliqué sur la flèche gauche");
 })
 
+/***Définition nombre de points en fonction du slides***/
+let slidesnumber = slides.length;
+let newdot;
+const dots = document.querySelector(".dots");
+
+for (let i = 0; i < slidesnumber; i++) {
+	newdot = document.createElement("div");
+	dots.appendChild(newdot);
+	newdot.classList.add("dot");
+}
+
+/***Modification du bulletpoint de la slide affichée***/
+const alldots = document.querySelectorAll(".dot");
+let slidesposition = 0; 
+
+for (let i = 0; i < alldots.length; i++) {
+	if (i === slidesposition) {
+		alldots[i].classList.add("dot_selected");
+	}
+	else {
+		alldots[i].classList.remove("dot_selected");
+	}
+}
